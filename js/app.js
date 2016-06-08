2921c5ee2b404fb6d66227f25c311ed64c8d2152
mcelearr <rorymcelearney@gmail.com>
2016-06-08 14:20:07 +0100

    Fixes classname bug in css

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
-    document.getElementById("mySidenav").className = "opensidenav";
+    document.getElementById("mySidenav").className = "sidenav opensidenav";
     document.getElementById("main").className = "mainopensidenav";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
 function closeNav() {
-    document.getElementById("mySidenav").className = "closedsidenav";
+    document.getElementById("mySidenav").className = "sidenav closedsidenav";
     document.getElementById("main").className = "mainclosedsidenav";
     document.body.style.backgroundColor = "white";
 }

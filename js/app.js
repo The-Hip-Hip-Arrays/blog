78a53fd9ac00fdc710e4324f67ab9f43957d910b
mcelearr <rorymcelearney@gmail.com>
2016-06-08 17:33:51 +0100

    try to add event listener for teamperson class

--- js/app.js
+++ js/app.js
@@ -12,16 +12,23 @@ function closeNav() {
     document.body.style.backgroundColor = "white";
 }
 
-function closeImage() {
-      this.style.display = 'none';
+function closeImage(event) {
+    console.log(event.target);
+    var target = event.target;
+
+    if (target.className.split(' ').indexOf('teamperson') !== -1) {
+      target.className = 'teamperson show';
+    }
 }
 
 
 var openMenuBtn = document.querySelector('.js-open-btn'),
-closeMenuBtn = document.querySelector('.js-close-btn');
-closeImageBtn = document.querySelector('.img-circle');
+ closeMenuBtn = document.querySelector('.js-close-btn'),
+ closeImageBtns = document.querySelectorAll('.teamperson');
 
+for (var i = 0; i < 4; i++){
+  closeImageBtns[i].addEventListener('click', closeImage, false);
+}
 
 openMenuBtn.addEventListener('click', openNav);
 closeMenuBtn.addEventListener('click', closeNav);
-closeImageBtn.addEventListener('click', closeImage);

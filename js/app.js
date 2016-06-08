e32a2e6e074f4b94fd2f7fcfd9711ac108648489
Shadows666 <lifeloveregretsxe@gmail.com>
2016-06-08 15:53:50 +0100

    Update to HTML

--- js/app.js
+++ js/app.js
@@ -1,13 +1,20 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("mySidenav").className = "sidenav opensidenav";
+    document.getElementById("main").className = "mainopensidenav";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("mySidenav").className = "sidenav closedsidenav";
+    document.getElementById("main").className = "mainclosedsidenav";
     document.body.style.backgroundColor = "white";
 }
+
+
+var openMenuBtn = document.querySelector('.js-open-btn'),
+closeMenuBtn = document.querySelector('.js-close-btn');
+
+openMenuBtn.addEventListener('click', openNav);
+closeMenuBtn.addEventListener('click', closeNav);

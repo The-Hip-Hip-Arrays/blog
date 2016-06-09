fa2f40bef34ce67d78af65c01b043629bcbdc8e5
Shadows666 <lifeloveregretsxe@gmail.com>
2016-06-09 15:25:44 +0100

    Add burger icon and fix colour sidebar issue

--- js/app.js
+++ js/app.js
@@ -1,67 +1,36 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
-    document.getElementById("mySidenav").className = "sidenav opensidenav";
-    document.getElementById("main").className = "mainopensidenav";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    $u('#mySidenav')
+      .removeClass('closedsidenav')
+      .addClass('opensidenav');
+
+    $u('#main')
+      .removeClass('mainclosedsidenav')
+      .addClass('mainopensidenav');
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
 function closeNav() {
-    document.getElementById("mySidenav").className = "sidenav closedsidenav";
-    document.getElementById("main").className = "mainclosedsidenav";
-    document.body.style.backgroundColor = "white";
-}
+    $u('#mySidenav')
+      .removeClass('opensidenav')
+      .addClass('closedsidenav');
 
-function $u(target){
-  var getClasses = function(){
-    return target.className.split(/\s+/);
-  };
-
-  var classNameIndex = function(className) {
-    return getClasses().indexOf(className);
-  };
-
-  var hasClass = function(className) {
-    return classNameIndex(className) !== -1;
-  };
-
-  var removeClass = function(className) {
-    var index = classNameIndex(className);
-    return target.className = getClasses().slice(0, index).concat(getClasses().slice(index + 1)).join(' ');
-  };
-
-  var addClass = function(className) {
-    if (hasClass(className)) return;
-    return target.className = [className].concat(getClasses()).join(' ');
-  };
-
-  var toggleClass = function(className) {
-    return hasClass(className) ? removeClass(className) : addClass(className);
-  };
-
-  return {
-    getClasses: getClasses,
-    hasClass: hasClass,
-    removeClass: removeClass,
-    addClass: addClass,
-    toggleClass: toggleClass
-  };
+    $u('#main')
+      .removeClass('mainopensidenav')
+      .addClass('mainclosedsidenav');
 }
 
 function toggleImage(event) {
-    var target = event.currentTarget;
+  var target = event.currentTarget;
 
-    if (!$u(target).hasClass('teamperson')) return;
+  if ($u(target).hasClass('teamperson')) {
     $u(target).toggleClass('toggle');
+  }
 }
 
-var openMenuBtn = document.querySelector('.js-open-btn'),
- closeMenuBtn = document.querySelector('.js-close-btn'),
- toggleImageBtns = document.querySelectorAll('.teamperson');
+$u('.teamperson').forEach(function(el){
+  $u(el).click(toggleImage);
+});
 
-for (var i = 0; i < toggleImageBtns.length; i++){
-  toggleImageBtns[i].addEventListener('click', toggleImage);
-}
-
-openMenuBtn.addEventListener('click', openNav);
-closeMenuBtn.addEventListener('click', closeNav);
+$u('.js-open-btn').click(openNav);
+$u('.js-close-btn').click(closeNav);
